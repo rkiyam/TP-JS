6421db0e848901c1efee71b4fa05eb9539804ad0
rkiyam <r.kiyam@icloud.com>
2026-01-02 18:59:20 +0100

    attribution du score joueur et du temp de jeu

--- DunkHunt.js
+++ DunkHunt.js
@@ -1,12 +1,36 @@
 var titi = document.getElementById("titi");
 var container = document.getElementById("container");
-var scorejoueur = document.getElementById("scorejoueur")
-var temps = document.getElementById("temps")
+var scorejoueur = document.getElementById("scorejoueur");
+var temps = document.getElementById("temps");
+var scoretiti = document.getElementById("scoretiti");
 
 var mouvement = 1000;
 var score = 0;
 
-function mouvtiti() [
-    var Largeur = container.offsetWidth - titi.offsetWidth;
-    var Longueur = container.offsetHeight - titi.offsetHeight;
-]
+function mouvtiti() {
+    var Largeur = container.clientWidth - titi.clientWidth;
+    var Longueur = container.clientHeight - titi.clientHeight;
+    var Lar = Math.random() * Largeur;
+    var Long = Math.random() * Longueur;
+
+    titi.style.left=Lar + "px";
+    titi.style.top=Long + "px";
+}
+
+var deplacement = setInterval(mouvtiti,mouvement);
+
+titi.addEventListener("click", ()=>{
+    score++; 
+    scorejoueur.textContent= score;
+    mouvtiti
+})
+
+var temps = 30;
+setInterval(() =>{
+    temps--;
+    if(temps===0) {
+        location.reload();
+    }
+    
+},1000);
+
