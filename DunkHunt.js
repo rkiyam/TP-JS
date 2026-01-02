e75bde353959027eae259b030075ae06068b6eef
rkiyam <r.kiyam@icloud.com>
2026-01-02 18:10:57 +0100

    Deuxième Commit

--- DunkHunt.js
+++ DunkHunt.js
@@ -1,4 +1,12 @@
 var titi = document.getElementById("titi");
 var container = document.getElementById("container");
 var scorejoueur = document.getElementById("scorejoueur")
+var temps = document.getElementById("temps")
 
+var mouvement = 1000;
+var score = 0;
+
+function mouvtiti() [
+    var Largeur = container.offsetWidth - titi.offsetWidth;
+    var Longueur = container.offsetHeight - titi.offsetHeight;
+]
